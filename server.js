c99eaaf756f8a3011ede1eefe7ddb0e2194e0c26
Purpemaster <lukas.gastl@icloud.com>
2025-04-12 07:30:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-// Express server placeholder
-console.log('Server läuft...');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('PURPE Wallet Tracker läuft!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
